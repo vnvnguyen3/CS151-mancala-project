53d9bbb5e2c73a3e4975ab1329140646d4f520e8
My Nguyen <kieumynguyen94@gmail.com>
2018-05-02 23:30:08 -0700

    Add files via upload

--- CapturePit.java
+++ CapturePit.java
@@ -1,26 +1,37 @@
-package mancala;
-/*
- * @author Vincent Nguyen
+/**
+ * Team Project Solution
+ * Author: Vincent Nguyen, My Nguyen, Seng Cheok
+ * Version: Coding in Java Language
+ * Copyright
+ */
+package project.CS151;
+
+/**
+ * CapturePit class
+ * @author Vincent Nguyen, My Nguyen, Seng Cheok
+ * Postcondition: Store all the stones that each player wins in their own Mancala
  */
 public class CapturePit extends Pit{
-	/*
-	 * CapturePit class
-	 */
+	
+	//constructor
 	public CapturePit(int s) {
 		super(s);
 	}
-	/*
-	 * Gathers all of the stones from pits
+	/**
+	 * gather function
 	 * @param s the number of stones from the pits
+	 * Postcondition: Gathers all of the stones from pits
 	 */
 	public void gather(int s) {
 		super.add(s);
 	}
-	/*
-	 * Return true if this is the capture pit
+	/**
+	 * isCapture function
 	 * @return true
+	 * Postcondition: Return true if this is the capture pit
 	 */
 	public boolean isCapture() {
 		return true;
 	}
-}
+
+}//end CapturePit class
